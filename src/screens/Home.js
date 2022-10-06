ed86d4d7224ac950d68c6317d2be584d45424b81
Leo Augusto Kraisch <leonardokraisch@gmail.com>
2022-10-06 15:55:41 -0300

    carteiras

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,6 +5,7 @@ import MiniWallet from "../components/MiniWallet";
 import MainWallet from "../components/Wallet/MainWallet";
 import AddView from "../components/AddLaunch/AddView";
 import useMoney from "../data/hooks/useMoney";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default props => {
     const { getWallets } = useMoney()
@@ -28,12 +29,18 @@ export default props => {
                     width='97%'
                     data={wallets}
                     renderItem={(wallet) =>
-                        <MiniWallet 
-                        wallet={wallet} 
-                        navigation={props.navigation}
+                        <MiniWallet
+                            wallet={wallet}
+                            navigation={props.navigation}
                         />
                     }
                 />
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.newButton}>
+                        <FontAwesome5 name="plus" size={25} color="#FFF" />
+                        <Text style={styles.newButtonText}>Create Wallet</Text>
+                    </TouchableOpacity>
+                </View>
                 <AddView />
             </View>
             <StatusBar backgroundColor={'#32779E'} />
@@ -61,5 +68,39 @@ const styles = StyleSheet.create({
         height: '30%',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    buttonContainer: {
+        justifyContent: 'flex-start',
+        maxHeight: '16%',
+        width: '93%',
+        borderTopColor: '#FFF',
+        borderTopWidth: 2
+
+    },
+    newButton: {
+        marginTop: 7,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '42%',
+        height: '70%',
+        padding: 10,
+        borderRadius: 5,
+        flexWrap: 'wrap',
+        backgroundColor: '#32779E',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 10
+    },
+    newButtonText: {
+        color: '#FFF',
+        fontSize: 16,
+        fontWeight: 'bold',
+        paddingLeft: 4,
     }
 })
