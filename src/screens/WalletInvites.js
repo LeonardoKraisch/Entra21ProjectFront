c4cb7644f1c8a9232402fb47cbaeb05885de94b7
André Feuzer <andre.heydran@gmail.com>
2022-10-07 15:30:01 -0300

    arrumando rotas de invite

--- src/screens/WalletInvites.js
+++ src/screens/WalletInvites.js
@@ -4,13 +4,18 @@ import { Linking, Text, TouchableOpacity, View } from 'react-native';
 import QRCode from 'react-native-qrcode-svg'
 
 export default props => {
-    const wallet = props.navigation.getState().routes[3].params.wallet.split(".p.")
+    var inviteRoute = null
+    props.navigation.getState().routes.forEach(route => {
+        if (route.name == "WalletInvites")
+        inviteRoute = route
+    })
+    const wallet = inviteRoute.params.wallet.split(".p.")
     const walletCode = wallet[0]
     const walletName = wallet[1]
     return (
         <View>
             <TouchableOpacity>
-                <Text> Você deseja registrar a Wallet:{walletName} </Text>
+                <Text> Você deseja registrar a Wallet:{null} </Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => Linking.openURL(createURL("/Home"))}>
                 <Text>Voltar</Text>
