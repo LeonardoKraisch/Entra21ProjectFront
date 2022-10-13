b300fb867d61a49bb6984b2b49bd93ba3ce12df1
André Feuzer <andre.heydran@gmail.com>
2022-10-13 19:21:40 -0300

    retirando logs de debug

--- src/data/contexts/MoneyContext.js
+++ src/data/contexts/MoneyContext.js
@@ -164,7 +164,6 @@ export const MoneyProvider = ({ children }) => {
 
         getRegisters: async data => {
             data["user"] = { code: userCode }
-            console.log(data.type, "data typeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
             const newQuery = await axios.post(`/${data.type == "+" ? "income" : "expense"}/query`,
                 data)
             return newQuery.data.registers
@@ -357,13 +356,10 @@ export const MoneyProvider = ({ children }) => {
 
         calcTotal: async (array, camp) => {
             var total = 0
-            console.log(array,"array -------------------------------------------------------------------------", camp);
             try {
                 array.forEach((element) => {
                     total += element[camp]
-                    console.log(element[camp])
                 })
-                console.log(total,"total")
                 return total
             } catch (e) {
                 return total
